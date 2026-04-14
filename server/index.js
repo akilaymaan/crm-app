91a54f9191fc70117149d61650a12924e9fa13bb
Akil Aymaan <akilaymaan@gmail.com>
2026-04-15 00:26:50 +0530

    Update CORS configuration to allow Vercel origins

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174'], credentials: true }));
+app.use(cors({
+  origin: process.env.CLIENT_URL || '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
